f246a8c84a7b64d671698c4497e2dfad1b1beb98
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-14 15:15:06 -0400

    change: successfully implemented basic universal banking detection

--- src/main/java/facemod/mixins/HandledScreenMixin.java
+++ src/main/java/facemod/mixins/HandledScreenMixin.java
@@ -1,6 +1,7 @@
 package facemod.mixins;
 
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +13,13 @@ public abstract class HandledScreenMixin {
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo info) {
 
-        System.out.println("Container opened: " + this.getClass().getName());
+        HandledScreen<?> handledScreen = (HandledScreen<?>) (Object) this;
+        Text screenTitle = handledScreen.getTitle();
+
+        System.out.println("Container Name: " + screenTitle.getString());
+        //TODO: Replace this hard coded unicode character with class.
+        if (screenTitle.getString().contains("拴")) {
+            System.out.println("Bank Opened");
+        }
     }
 }

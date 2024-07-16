4d25a956a4064408b7d9208c1f754cd97569338a
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-16 12:31:25 -0400

    change: clean up.

--- src/main/java/facemod/screens/BankScreen.java
+++ src/main/java/facemod/screens/BankScreen.java
@@ -2,8 +2,6 @@ package facemod.screens;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.tooltip.Tooltip;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public class BankScreen extends Screen {
@@ -19,17 +17,7 @@ public class BankScreen extends Screen {
 
     @Override
     protected void init() {
-        ButtonWidget button1 = ButtonWidget.builder(Text.literal("Button 1"), button -> System.out.println("You clicked button1!"))
-                .dimensions(width / 2 - 205, 20, 200, 20)
-                .tooltip(Tooltip.of(Text.literal("Tooltip of button1")))
-                .build();
-        ButtonWidget button2 = ButtonWidget.builder(Text.literal("Button 2"), button -> System.out.println("You clicked button2!"))
-                .dimensions(width / 2 + 5, 20, 200, 20)
-                .tooltip(Tooltip.of(Text.literal("Tooltip of button2")))
-                .build();
-
-        //addDrawableChild(button1);
-        //addDrawableChild(button2);
+        System.out.println("Bank Screen");
     }
 
     @Override

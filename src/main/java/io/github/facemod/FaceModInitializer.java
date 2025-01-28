902dac3aa19d74353516020719e117d3b1d838f6
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2025-01-28 10:36:02 -0500

    change: added faceland checks - Spade

--- src/main/java/io/github/facemod/FaceModInitializer.java
+++ src/main/java/io/github/facemod/FaceModInitializer.java
@@ -6,9 +6,13 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.ClientConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class FaceModInitializer implements ClientModInitializer {
     public final Logger logger = LoggerFactory.getLogger(FaceModInitializer.class);
@@ -26,10 +30,22 @@ public class FaceModInitializer implements ClientModInitializer {
         CONFIG = holder.getConfig();
         FaceConfig.holder = holder;
 
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> client.execute(() -> {
+            ClientConnection connection = Objects.requireNonNull(client.getNetworkHandler()).getConnection();
+            if (connection != null && connection.getAddress() != null) {
+                String serverAddress = connection.getAddress().toString().toLowerCase();
+                FaceConfig.General.onFaceLand = serverAddress.startsWith("local") || serverAddress.contains("face.land");
+            }
+        }));
+
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+            FaceConfig.General.onFaceLand = false;
+        });
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            /*if(!onFaceland){
+            if (!FaceConfig.General.onFaceLand || client == null || client.player == null) {
                 return;
-            }*/
+            }
 
             Mount.update();
 

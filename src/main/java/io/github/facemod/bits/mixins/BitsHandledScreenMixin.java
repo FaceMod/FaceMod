39a52233fd9877924db5515c301e64bba5cb8218
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2025-07-06 16:11:39 -0400

    fish seller wip. - Spade

--- src/main/java/io/github/facemod/bits/mixins/BitsHandledScreenMixin.java
+++ src/main/java/io/github/facemod/bits/mixins/BitsHandledScreenMixin.java
@@ -1,18 +1,12 @@
 package io.github.facemod.bits.mixins;
 
 import io.github.facemod.FaceModInitializer;
-import io.github.facemod.bank.screens.BankScreen;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Objects;
 
 @Mixin(HandledScreen.class)
 public abstract class BitsHandledScreenMixin {
